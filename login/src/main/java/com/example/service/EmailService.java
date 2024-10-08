64d9c0e0a368ad854bab45aa5c1bf62c30826ba6
EthanDCR <clinteth.dev@gmail.com>
2024-10-07 22:45:35 -0500

    commented out some changes i made trying to add an email service controller becasue i was having issues configuring it with the application to get it to a working state.

--- login/src/main/java/com/example/service/EmailService.java
+++ login/src/main/java/com/example/service/EmailService.java
@@ -1,24 +1,24 @@
 
 package com.example.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.mail.SimpleMailMessage;
+// import org.springframework.mail.javamail.JavaMailSender;
 
 @Service
 public class EmailService {
 
-  @Autowired
-  private JavaMailSender mailSender;
+  // @Autowired
+  // private JavaMailSender mailSender;
 
-  public void sendSimpleEmail(String to, String subject, String body) {
-    SimpleMailMessage message = new SimpleMailMessage();
-    message.setTo(to);
-    message.setSubject(subject);
-    message.setText(body);
-    message.setFrom("clinteth.dev@gmail.com");
+  // public void sendSimpleEmail(String to, String subject, String body) {
+  // SimpleMailMessage message = new SimpleMailMessage();
+  // message.setTo(to);
+  // message.setSubject(subject);
+  // message.setText(body);
+  // message.setFrom("clinteth.dev@gmail.com");
 
-    mailSender.send(message);
-  }
+  // mailSender.send(message);
+  // }
 }
